a4b40b8d9d084d9944e1c61b54b632b3adef4457
luxardOri <127153726+luxardOri@users.noreply.github.com>
2023-09-04 19:38:06 -0700

    updated user model

--- models/User.js
+++ models/User.js
@@ -1,21 +1,31 @@
 const { Schema, model } = require("mongoose");
-const thoughtSchema = require("./Thought");
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Invalid email address"],
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: "thought" }],
+    friends: [{ type: Schema.Types.ObjectId, ref: "user" }],
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-  },
-  thoughts: [thoughtSchema],
-  friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
 
 const User = model("user", userSchema);
